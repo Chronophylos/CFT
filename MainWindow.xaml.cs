e3bb63f27a76bace024c4e2a7cb42b89f186b11f
Nikolai <Chronophylos@users.noreply.github.com>
2016-11-17 13:52:00 +0100

    made MainWindow.xaml.cs more talkactive

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,7 +15,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using __Cereal__;
 
-
 namespace CerealFileTransfer {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
@@ -23,22 +22,45 @@ namespace CerealFileTransfer {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            Cereal rs232 = new Cereal("COM1", 9600, 8, StopBits.One, Parity.None);
+            string portName = "COM1";
+            int baudrate = 9600
+            int dataBits = 8;
+            StopBits stopBits = StopBits.One;
+            Parity parity = Parity.None
+            Cereal rs232 = new Cereal(portName, baudrate, dataBits, stopBits, parity);
+            rtb_Log.AppendText("[ INFO ] Created Serial Port Driver on " + portName +
+                               " with " + dataBits " databits, " + stopBits " and " + 
+                               stopBits + " at a speed of " + baudrate " bps");
 
-            bool retryCOM1 = true;
-            while (retryCOM1) {
+            /* open COM1
+            Display a Error MessageBox when open() return false
+                OK to retry opening COM1
+                CANCEL to close the program
+            */
+            //bool retryCOM1 = true;
+            //while (retryCOM1) {
+            while(true) {
                 if (!rs232.open()) {
-                    switch (MessageBox.Show("ERROR: Can't open COM1\nRetry to open COM1?",
-                                            "Error opening COM1",
+                    switch (MessageBox.Show("ERROR: Can't open " + portName + ".\n" +
+                                            "Maybe " + portName + " is in use or does not exist\n" +
+                                            "Retry opening " + portName + "?",
+                                            "Error opening " + portName,
                                             MessageBoxButton.OKCancel,
                                             MessageBoxImage.Asterisk,
                                             MessageBoxResult.OK)) {
                         case MessageBoxResult.OK: break;
                         case MessageBoxResult.Cancel: return;
                     } 
-                } else { retryCOM1 = false; }
+                } else { break; //retryCOM1 = false; }
             }
-            rtb_Log.AppendText("[  OK  ] Port COM1 opened");
+            rtb_Log.AppendText("[  OK  ] Port " + portName + " opened");
+
+            rs232.setDTR(true); // we're ready to work
+            rtb_Log.AppendText("[ INFO ] Terminal ready");
+            // are you ready too?
+            while (!rs232.isDCD()); // we're waiting
+            rtb_Log.AppendText("[  OK  ] Partner ready");
+            // now it is the GUIs turn, we're done here
         }
     }
 }
