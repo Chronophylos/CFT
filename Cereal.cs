88fa3af390ee245ae7b6fc5c01d617b541f8dc81
Chronophylos <nikolai.zimmermann@web.de>
2016-11-19 19:19:45 +0100

    moved Serial Port Init to CFT_Loaded and cleaned Cereal.cs

--- Cereal.cs
+++ Cereal.cs
@@ -3,29 +3,31 @@ using System.IO.Ports;
 
 namespace __Cereal__ {
     class Cereal {
-        private string portName;
-        private int baudrate;
-        private int dataBits;
+        private String portName;
+        private Int32 baudrate;
+        private Int32 dataBits;
         private StopBits stopBits; // Der Anzahl der Stoppbits (Stopbits.One, StopBits.OnePointFive, StopBits.Two)
         private Parity parity; // Festlegung der Parität (Parity.Even, Parity.Mark, Parity.None, Parity.Odd, Parity.Space)
         private SerialPort sp;
         
-        public Cereal(string portName, int baudrate, int dataBits, StopBits stopBits, Parity parity) {
+        public Cereal(String portName, Int32 baudrate, Int32 dataBits, StopBits stopBits, Parity parity) {
             this.portName = portName;
             this.baudrate = baudrate;
             this.dataBits = dataBits;
             this.stopBits = stopBits;
             this.parity = parity;
 
-            sp = new SerialPort();
-            sp.PortName = portName;
-            sp.BaudRate = baudrate;
-            sp.DataBits = dataBits;
-            sp.StopBits = stopBits;
-            sp.Parity = parity;
+            sp = new SerialPort()
+            {
+                PortName = portName,
+                BaudRate = baudrate,
+                DataBits = dataBits,
+                StopBits = stopBits,
+                Parity = parity
+            };
         }
-        
-        public bool open() {
+
+        public Boolean Open() {
             try { sp.Open(); }
             catch(UnauthorizedAccessException){ return false; }
             catch (ArgumentOutOfRangeException){ return false; }
@@ -33,29 +35,29 @@ namespace __Cereal__ {
             catch (InvalidOperationException){ return false; }
             return true;
         }       
-        public void close(){ sp.Close(); }
+        public void Close(){ sp.Close(); }
         
-        public int dataAvailable(){ return sp.BytesToRead; } // Anzahl Zeichen, die im Empfangspuffer stehen
+        public Int32 DataAvailable(){ return sp.BytesToRead; } // Anzahl Zeichen, die im Empfangspuffer stehen
         
-        public int read(){ return sp.ReadByte(); }        
-        public int read(System.Byte[] buffer, int bufSize){ return sp.Read(buffer, 0, bufSize); }
-        public string readLine(){ return sp.ReadLine(); }
+        public Int32 Read(){ return sp.ReadByte(); }        
+        public Int32 Read(Byte[] buffer, Int32 bufSize){ return sp.Read(buffer, 0, bufSize); }
+        public String ReadLine(){ return sp.ReadLine(); }
         
-        public void write(int value){
+        public void Write(Int32 value){
             //System.Byte[] b = new System.Byte[1];
             //b[0] = (byte)value;
             //sp.Write(b, 0, 1);
             sp.Write(BitConverter.GetBytes(value), 0 , 1);
         }     
-        public void write(System.Byte[] buffer, int bytesToWrite){ sp.Write(buffer, 0, bytesToWrite); }
-        public void write(string s){ sp.Write(s); }    
+        public void Write(Byte[] buffer, Int32 bytesToWrite){ sp.Write(buffer, 0, bytesToWrite); }
+        public void Write(String s){ sp.Write(s); }    
         
-        public void setRTS(bool arg){ sp.RtsEnable = arg; }
-        public void setDTR(bool arg){ sp.DtrEnable = arg; }        
-        public bool isCTS(){ return sp.CtsHolding; }        
-        public bool isDSR(){ return sp.DsrHolding; }
-        public bool isDCD(){ return sp.CDHolding; }
-        public bool isDTR(){ return sp.DtrEnable; }
-        public bool isRTS(){ return sp.RtsEnable; }
+        public void SetRTS(Boolean arg){ sp.RtsEnable = arg; }
+        public void SetDTR(Boolean arg){ sp.DtrEnable = arg; }        
+        public Boolean IsCTS(){ return sp.CtsHolding; }        
+        public Boolean IsDSR(){ return sp.DsrHolding; }
+        public Boolean IsDCD(){ return sp.CDHolding; }
+        public Boolean IsDTR(){ return sp.DtrEnable; }
+        public Boolean IsRTS(){ return sp.RtsEnable; }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,18 +19,46 @@ namespace CerealFileTransfer {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
+    public partial class MainWindow : Window {
+        private String portName;
+        private Int32 baudrate;
+        private Int32 dataBits;
+        private StopBits stopBits;
+        private Parity parity;
+        private Cereal rs232;
+
         public MainWindow() {
             InitializeComponent();
-            string portName = "COM1";
-            int baudrate = 9600
-            int dataBits = 8;
-            StopBits stopBits = StopBits.One;
-            Parity parity = Parity.None
-            Cereal rs232 = new Cereal(portName, baudrate, dataBits, stopBits, parity);
-            rtb_Log.AppendText("[ INFO ] Created Serial Port Driver on " + portName +
-                               " with " + dataBits " databits, " + stopBits " and " +
-                               stopBits + " at a speed of " + baudrate " bps");
+            this.portName = "COM1";
+            this.baudrate = 9600;
+            this.dataBits = 8;
+            this.stopBits = StopBits.One;
+            this.parity = Parity.None;
+            this.rs232 = new Cereal(portName, baudrate, dataBits, stopBits, parity);
+        }
 
+        // convert control and data to a package ready to send
+        Byte[] StringToPackage(String control, String data) {
+            Byte[] controlBytes = new Byte[4];
+            System.Buffer.BlockCopy(control.ToCharArray(), 0,
+                                    controlBytes, 0, 4);
+            Int32 dataSize = data.Length * sizeof(Char);
+            Byte[] dataSizeBytes = new Byte[4];
+            dataSizeBytes = BitConverter.GetBytes(dataSize);
+            Byte[] dataBytes = new Byte[dataSize];
+            System.Buffer.BlockCopy(data.ToCharArray(), 0,
+                                    dataBytes, 0, 4);
+
+            Byte[] package = new Byte[4 + 4 + dataSize];
+            package = controlBytes.Concat(dataSizeBytes.Concat(dataBytes).ToArray()).ToArray();
+            /*package = controlBytes;
+            package.AddRange(dataSizeBytes);
+            package.AddRange(dataBytes);*/
+
+            return package;
+        }
+
+        private void CFT_Loaded(Object sender, RoutedEventArgs e) {
             /* open COM1
             Display a Error MessageBox when open() return false
                 OK to retry opening COM1
@@ -39,47 +67,41 @@ namespace CerealFileTransfer {
             //bool retryCOM1 = true;
             //while (retryCOM1) {
             while(true) {
-                if (!rs232.open()) {
-                    switch (MessageBox.Show("ERROR: Can't open " + portName + ".\n" +
+                if(!rs232.Open()) {
+                    switch(MessageBox.Show("ERROR: Can't open " + portName + ".\n" +
                                             "Maybe " + portName + " is in use or does not exist\n" +
                                             "Retry opening " + portName + "?",
                                             "Error opening " + portName,
                                             MessageBoxButton.OKCancel,
-                                            MessageBoxImage.Asterisk,
+                                            MessageBoxImage.Error,
                                             MessageBoxResult.OK)) {
-                        case MessageBoxResult.OK: break;
-                        case MessageBoxResult.Cancel: return;
+                        case MessageBoxResult.OK:
+                            break;
+                        case MessageBoxResult.Cancel:
+                            return;
                     }
-                } else { break; //retryCOM1 = false; }
+                } else { break; }
             }
-            rtb_Log.AppendText("[  OK  ] Port " + portName + " opened");
+            rtb_Log.AppendText("[  OK  ] Port " + portName + " opened\n");
 
-            rs232.setDTR(true); // we're ready to work
-            rtb_Log.AppendText("[ INFO ] Terminal ready");
+            rs232.SetDTR(true); // we're ready to work
+            rtb_Log.AppendText("[ INFO ] Terminal ready\n");
             // are you ready too?
-            while (!rs232.isDCD()); // we're waiting
-            rtb_Log.AppendText("[  OK  ] Partner ready");
+            while(!rs232.IsDCD()) {
+                switch(MessageBox.Show("ERROR: Cannot connect with Partner\n" +
+                                       "Maybe the cable is not connected correctly?",
+                                       "Error connecting to Partner",
+                                       MessageBoxButton.OKCancel,
+                                       MessageBoxImage.Error,
+                                       MessageBoxResult.OK)) {
+                    case MessageBoxResult.OK:
+                        break;
+                    case MessageBoxResult.Cancel:
+                        return;
+                } // we're waiting
+            }
+            rtb_Log.AppendText("[  OK  ] Partner ready\n");
             // now it is the GUIs turn, we're done here
-        }
-
-        // convert control and data to a package ready to send
-        stringToPackage(string control, string data){
-            byte[] controlBytes = new byte[4];
-            System.Buffer.BlockCopy(control.ToCharArray(), 0,
-                                    controlBytes, 0, 4);
-            int dataSize = data.Length * sizeof(char);
-            byte[] dataSizeBytes = new byte[4](BitConverter.GetBytes(dataSize));
-            byte[] dataBytes = new byte[dataSize];
-            System.Buffer.BlockCopy(data.ToCharArray(), 0,
-                                    dataBytes, 0, 4);
-
-            byte[] package = new byte[4 + 4 + dataSize];
-            package = controlBytes.Concat(dataSizeBytes.Concat(dataBytes).ToArray()).ToArray();
-            /*package = controlBytes;
-            package.AddRange(dataSizeBytes);
-            package.AddRange(dataBytes);*/
-
-            return package;
         }
     }
 }
